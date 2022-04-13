3c5fa5bde407b9578ebb2c37c6652bf5c235675f
uehara tomoyuki <22uehara_tomoyuki@axiz.co.jp>
2022-04-13 18:56:17 +0900

    kansei

--- db_exam1.sql
+++ db_exam1.sql
@@ -2,14 +2,14 @@
 \c db_exam
 create table major(
     major_id int primary key
-    , major_name varachar(50)
+    , major_name varchar(50)
 ) ;
 create table student(
     student_id int primary key
-    ,student_name varachar(50)
+    ,student_name varchar(50)
     ,grade int 
-    ,hometown varachar(50)
-    ,major_id int not nullreferences major(major_id)
+    ,hometown varchar(50)
+    ,major_id int not null references major(major_id)
 ) ;
 insert into major
 (major_id,major_name) 
@@ -30,11 +30,11 @@ values(1,'山田',1,'宮城',1),
       (7,'伊藤',3,'鹿児島',2),
       (8,'山本',3,'神奈川',3),
       (9,'森本',4,'沖縄',4),
-      (10,'吉岡',4,'神奈川',5),
+      (10,'吉岡',4,'神奈川',5)
       ;
 select * from student where grade = 1 ;
 select * from student where hometown = '東京' ;
-select major_name from student ;
+select major_name from major ;
 update student set grade = 3 where student_id = 1 ;
 delete from student where student_id = 10 ;
       
