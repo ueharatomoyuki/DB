d95ca5c9ade38a3539c3bbf65e504ac4cca68e45
uehara tomoyuki <22uehara_tomoyuki@axiz.co.jp>
2022-04-14 16:16:54 +0900

    ensyu 2

--- db_exam2.sql
+++ db_exam2.sql
@@ -12,3 +12,42 @@ order by student_id desc ;
 select major_id , max(grade)  from student
 group by major_id
 order by major_id ;
+
+4
+select hometown , count(hometown) from student 
+group by hometown
+having count(hometown) >= 2 
+order by hometown ;
+
+5
+select s. student_name , m.major_name
+from student s
+inner join major m
+on s.major_id = m.major_id
+order by major_name , student_name;
+
+6
+select student_id , s. student_name , hometown , m.major_name
+from student s
+inner join major m 
+on s.major_id = m.major_id
+where hometown <> '東京'
+order by major_name , student_id;
+
+7
+select student_id , student_name , grade
+from student
+where major_id = (select major_id
+                   from major
+                   where major_name = '英文学')
+order by student_id;
+
+8
+select student_id, student_name, major_id
+from student s 
+where  s.major_id in (select major_id
+            from student
+            group by major_id
+            having count(major_id) >= 3)
+order by major_id , student_id
+;            
